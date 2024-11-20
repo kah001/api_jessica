205491e3294fabd9cfa5f076b8866b1de3174bb8
fellpos <felipesilvafrancotti3@gmail.com>
2024-11-20 10:19:43 -0300

    leve mudança no controller do projeto em andamento para permitir o envio de imagens pelo app

--- src/controller/projetoAndamentoController.js
+++ src/controller/projetoAndamentoController.js
@@ -5,21 +5,30 @@ import { Router } from "express";
 
 const endpoints = Router();
 
-endpoints.post('/projeto/andamento', autenticar, async (req, resp) => {
+endpoints.post('/projeto/andamento', async (req, resp) => {
     try {
         let projetoAndamento = req.body;
+
+        // Se a imagem for fornecida, converte de base64 para buffer
+        if (projetoAndamento.imagem) {
+            const imagemBase64 = projetoAndamento.imagem.replace(/^data:image\/[a-zA-Z]*;base64,/, "");
+            const buffer = Buffer.from(imagemBase64, 'base64');
+
+            // Atualiza o campo da imagem para o buffer
+            projetoAndamento.imagem = buffer;
+        }
+
+        // Chama o serviço para salvar no banco
         let id = await service.inserirProjetoAndamentoService(projetoAndamento);
 
         resp.send({
             novoId: id
-        })
+        });
+    } catch (err) {
+        resp.status(400).send({ erro: err.message });
     }
-    catch (err) {
-        resp.status(400).send({
-            erro: err.message
-        })
-    }
-})
+});
+
 
 endpoints.get('/projetos/andamento/:id', autenticar, async (req, resp) => {
     try {
